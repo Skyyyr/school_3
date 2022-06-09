64b1092f01a1bc1c421076a77061f803ad9bfd2a
Skyyyr <emuswgluvr@gmail.com>
2022-06-09 11:08:38 -0700

    First

--- runner.py
+++ runner.py
@@ -3,7 +3,8 @@ from classes.school import School
 school = School('Ridgemont High') 
 
 while True:
-    mode = input("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n")
+    mode = input("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student "
+                 "<student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n")
 
     if mode == '1':
         school.list_students()
